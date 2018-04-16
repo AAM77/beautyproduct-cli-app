b20490138a79c6c35fd8306522c11f67d0dba889
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2018-04-16 11:43:50 -0400

    Created conditions for account for missing product_info (nil). Still needs some correction.

--- lib/beauty_product/scraper.rb
+++ lib/beauty_product/scraper.rb
@@ -35,11 +35,48 @@ class BeautyProduct::Scraper
   def self.scrape_product_page
     BeautyProduct::Product.all.each do |product|
       product_page = Nokogiri::HTML(open(product.url))
+      product_info = product_page.css(".productInfo.js-product-info ul li")
 
-      product.description = product_page.css(".productInfo.js-product-info ul li")[1].text
-      product.directions = product_page.css(".productInfo.js-product-info ul li")[2].text
-      product.ingredients = product_page.css(".productInfo.js-product-info ul li")[3].text
+      #product_info = product_page.css(".productInfo.js-product-info ul li")
+      product.price = product_page.css("span.productPrice.js-product-price").text.to_f ## ATTENTION:  .to_f  converting to float here!! ... Might cause problems later.
+
+      product_info.each do |info|
+        if info.css("div.itemHeader span").text == "Description"
+          product.description = info.css("div.itemContent p").text
+        elsif info.css("div.itemHeader span").text == "How to use"
+          product.directions = info.css("div.itemContent p").text
+        elsif info.css("div.itemHeader span").text == "Full ingredients list"
+          product.ingredients = info.css("div.itemContent p").text
+        end # |if|
+      end # |info|
+
+      ## ^^<#=^^ WORKING!! But, is assigning only the FIRST "p" -- FIX IT // FIX THIS
+      ## try doing a .collect on all of the paragraphs and assign the array to a single variable.
+      ## Then join the items in the array.
+      ## OR
+      ## try to do it directly  e.g. array.collect {|i| i}.join(" ") ... i.e. info.css("div.itemContent").collect { |p| p.text }.join(" ") (TRY IT)
+
+      #product.description = product_page.css(".productInfo.js-product-info ul li")[1].text
+      #product.directions = product_page.css(".productInfo.js-product-info ul li")[2].text
+      #product.ingredients = product_page.css(".productInfo.js-product-info ul li")[3].text
     end # |product|
   end # self.scrape_product_page
 
 end # class Scraper
+
+=begin
+product_info.each do |info|
+  if info.css("div.itemHeader span").text == "Description"
+    puts "DESCRIPTION!!!"
+    puts info.css("div.itemContent p").text
+  elsif info.css("div.itemHeader span").text == "How to use"
+    puts "INSTRUCTIONS!!!!"
+    puts info.css("div.itemContent p").text
+  elsif info.css("div.itemHeader span").text == "Full ingredients list"
+    puts "INGREDIENTS!!!"
+    puts info.css("div.itemContent p").text
+  end # |if|
+end # |info|
+=end
+
+## product_info = product_page.css(".productInfo.js-product-info ul li").text ##::CONTINUE:: YOU LEFT OFF HERE!!! -- This is a potential starting point for calling each "p"
