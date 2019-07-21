9f2fa9474f3e74e659a08a0f83f2b8a6d9ffe10a
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-20 21:20:41 -0400

    Created a new instace method, add_ingredients, to replace the class method, self.add_ingredients.

--- lib/beauty_product/product.rb
+++ lib/beauty_product/product.rb
@@ -21,29 +21,38 @@ class BeautyProduct::Product
     end # do |product|
   end # ingredient
 
-  # Creates an ingredient object for each one and adds this product to its products list
-  def self.add_ingredients
-    self.all.each do |product|
-      if !product.ingredients_array.nil?
-        product.ingredients_array.each do |ingredient|
-          if !BeautyProduct::Ingredient.all.detect {|element| element.name == ingredient}
-            new_ingredient = BeautyProduct::Ingredient.new(ingredient)
-            new_ingredient.products << product
-          else
-            existing_ingredient = BeautyProduct::Ingredient.all.detect {|element| element.name == ingredient}
-            existing_ingredient.products << product
-          end # if detect ingredient
-        end # each ingredient
-      end # if !nil?
-    end # each product
-  end # add_ingredient
+  # # Creates an ingredient object for each one and adds this product to its products list
+  # def self.add_ingredients
+  #   self.all.each do |product|
+  #     if !product.ingredients_array.nil?
+  #       product.ingredients_array.each do |ingredient|
+  #         if !BeautyProduct::Ingredient.all.detect {|element| element.name == ingredient}
+  #           new_ingredient = BeautyProduct::Ingredient.new(ingredient)
+  #           new_ingredient.products << product
+  #         else
+  #           existing_ingredient = BeautyProduct::Ingredient.all.detect {|element| element.name == ingredient}
+  #           existing_ingredient.products << product
+  #         end # if detect ingredient
+  #       end # each ingredient
+  #     end # if !nil?
+  #   end # each product
+  # end # add_ingredient
 
 
   def add_ingredients
-    if self.ingredients_array.nil?
-      
-    end
-  end
+    unless self.ingredients_array.nil?
+      self.ingredients_array.each do |ingredient|
+        unless BeautyProduct::Ingredient.all.detect {|element| element.name == ingredient}
+          new_ingredient = BeautyProduct::Ingredient.new(ingredient)
+          new_ingredient.products << self
+        else
+          existing_ingredient = BeautyProduct::Ingredient.all.detect {|element| element.name == ingredient}
+          existing_ingredient.products << self
+        end ## inner unless
+      end ## each
+    end ## outer unless
+  end ##  method
+
 end # class Product
 ####################
 ##  END OF CLASS  ##
